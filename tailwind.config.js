9161bcc64a84daec5f82952a2988d3f4532f0eaf
riponahmed <riponahmedptl2021@gmail.com>
2024-08-20 17:02:44 +0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
         dosis: ["dosis", "sans-serif"],
       },
       colors: {
-      'secondary':'#3D3E42',
-      'secondary-dark':'#31333B',
-      'yellow-light':'#FFD070',
+      'secondary':'#34353A',
+      'secondary-dark':'#262830',
+      'yellow-light':'#FFC86B',
       'dark-mate':'#31333B',
       'dark-light':'#9a9b9c',
       },
